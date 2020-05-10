7045265ac037bc41b30715044524474c96526de0
dxtrhza <dxtrhza@gmail.com>
2020-05-10 13:44:40 +0800

    First commit a1-conference-ph

---
+++ build/precache-manifest.b916b3811133ea68011ff445992cf2c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1127e9964f377f07997808ea0e24b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9148736bc52b80ce4b51",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76df10d13faab23292af",
+    "url": "/static/js/2.c4d91e52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4d91e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9148736bc52b80ce4b51",
+    "url": "/static/js/main.95172ccd.chunk.js"
+  },
+  {
+    "revision": "10378830b877d0dc26fb",
+    "url": "/static/js/runtime-main.bb5d7c87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
